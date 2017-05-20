a150ab8931494f086627bc8676f85f518c8b41d9
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2017-05-20 13:57:48 -0300

    Done: donuts

--- basic/string1.py
+++ basic/string1.py
@@ -24,8 +24,11 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    # +++your code here+++
-    return
+    n = count
+    if n >= 10:
+        n = 'many'
+
+    return "Number of donuts: {}".format(n)
 
 
 # B. both_ends
